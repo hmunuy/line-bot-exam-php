a9f5e435364984b7cbe1425e37494d4998c80ad5
hmunuy <50452855+hmunuy@users.noreply.github.com>
2019-05-09 21:02:39 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -5,7 +5,6 @@
 require "vendor/autoload.php";
 
 $access_token = 'g08oBecwnff9aIUo+1OzkLbb+j8RN/fiz65qfOiacinpq/3tRwkiAEFVohTrXvvV+lG9LouK0XZ8EoMwuRIrFpP+eMGqp1qH8wIZPqdUN1Q28o9+EAWliMWDJ3mtpj+cWWYW8Tq7Sy1ekvFDzbGi+wdB04t89/1O/w1cDnyilFU=';
-
 $channelSecret = '9362669de83b46da6003d3ba260ab460';
 
 $pushID = 'U680e60e2d3c43a4d0ac71d0bf5face98';
